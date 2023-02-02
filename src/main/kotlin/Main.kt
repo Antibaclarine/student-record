a000841703d54a10f7a7fc29157c8bd72f5b3378
clarine <antibaclarine.com>
2023-02-02 09:14:15 +0300

    making app

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    var fullname ="Antiba Clarine"
+     println(fullname)
+    var  age = 20
+    println(age)
+    var phonenumber = "0716102796"
+    println(phonenumber)
+    var weight = 30
+    println(weight)
+    var citizenship = "kenyan"
+    println(citizenship)
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
